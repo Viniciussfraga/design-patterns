0c6ad14f72c09351a63e1e2c50f8a34cd74094eb
Vinicius Fraga <viniciusfrg@outlook.com.br>
2024-07-29 22:36:37 -0300

    .

--- SimpleFactory/obj/Debug/net8.0/SimpleFactory.AssemblyInfo.cs
+++ SimpleFactory/obj/Debug/net8.0/SimpleFactory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c328a9273fba9691043fa77a3671470c0163cab0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81b55d9c31ef21164990dad814c45deaf76081c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
