2853030878b222f8791c74c5f52e8e4f1e497fab
Vinicius Fraga <viniciusfrg@outlook.com.br>
2024-08-02 00:55:40 -0300

    add FluentBuilder

---
+++ FluentBuilder/Program.cs
@@ -0,0 +1,11 @@
+﻿using FluentBuilder;
+
+var emailBuilder = new EmailBuilder()
+                    .To("enviar@gmail.com")
+                    .From("receber@gmail.com")
+                    .Subject("teste")
+                    .Body("Microsoft Build 2021\n\t25 a 27 de Maio 2021\n\tNovidades Windows,Azure")
+                    .CriarEmail();
+
+Console.WriteLine(emailBuilder.ToString());
+Console.ReadLine();
